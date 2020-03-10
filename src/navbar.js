0efe7a92cdf2b83ed1fcf9f24da836c60ce912c3
Jyn-Mun <jynmun@gmail.com>
2020-03-10 15:04:00 +0800

    modal menu

--- src/navbar.js
+++ src/navbar.js
@@ -1,14 +1,34 @@
-import React from "react";
-import { Button } from "react-bootstrap"
-
+import React, { useState } from "react";
+import { Button, Modal, NavLink } from "react-bootstrap"
+import { Link } from "react-router-dom"
 function Navbar() {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+
     return (
         <>
-            <Button class="navbar-toggler navbar-toggler-left rounded-0 border-0" type="button" data-toggle="collapse" data-target="#megamenu-dropdown" aria-controls="megamenu-dropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <i class="fa fa-bars"></i><span class="ml-3"></span>
+            <Button onClick={handleShow} className="barsModalButton">
+                <i class="fa fa-bars"></i><span></span>
             </Button>
+            <Modal show={show}>
+                <Modal.Header className="closebarsModalButton2">
+                    <Button onClick={handleClose} className="barsModalButton2">
+                        X
+                    </Button>
+                </Modal.Header>
+                <Modal.Body>
+                    <Modal.Title id="modalLinkDiv">
+                        <Link id="Link" tag={Link} to="/" onClick={handleClose}>Home</Link>
+                        <Link id="Link" tag={Link} to="/mood" onClick={handleClose}>Change Emotion</Link>
+                        <Link id="Link" tag={Link} to="/panic" onClick={handleClose}> Panic button</Link>
+                    </Modal.Title>
+                </Modal.Body>
+            </Modal>
         </>
     )
+
 
 }
 
