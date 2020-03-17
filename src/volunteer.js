c3f7e5ec7e04f297788d93c10c6629bd9e276ddf
Jyn-Mun <jynmun@gmail.com>
2020-03-17 12:03:10 +0800

    added volunteer page

--- src/volunteer.js
+++ src/volunteer.js
@@ -1,13 +1,36 @@
 import React from "react";
-import "./volunteer.css"
+import "./volunteer.css";
+import { Link } from "react-router-dom"
 
 function Volunteer() {
   return (
     <div className="container" id="VolunteerDiv">
       <div className="OrgLogoImage"></div>
-      <h6>befrienders.kl</h6>
+      <h6 className="BrandName">BefriendersKL</h6>
       <h6>helpline volunteer</h6>
-      <h6>03-7957-1306, admin@befrienders.org.my</h6>
+      <h6>03-7957-1306</h6>
+      <h6>admin@befrienders.org.my</h6>
+      <div className="VolunteerSocialMediaDiv">
+        <Link>
+          <h1 className="FacebookIcon"><i className="fab fa-facebook"></i><span></span></h1>
+        </Link>
+        <Link>
+          <h1 className="TwitterIcon"><i className="fab fa-twitter"></i><span></span></h1>
+        </Link>
+        <Link>
+          <h1 className="InstagramIcon"><i className="fab fa-instagram"></i><span></span></h1>
+        </Link>
+        <h6 className="SocialName">BefriendersKL</h6>
+      </div>
+      <button className="VolunteerButton">
+        <Link className="VolunteerLink">volunteer</Link>
+      </button>
+      <h6 className="VolunteerTitle">work needed</h6>
+      <h6>Lorem ipsum dolor sit amet</h6>
+      <h6 className="VolunteerTitle">when needed</h6>
+      <h6>Lorem ipsum dolor sit amet</h6>
+      <h6 className="VolunteerTitle">what will be provided (training)</h6>
+      <h6>Lorem ipsum dolor sit amet</h6>
     </div>
   )
 }
