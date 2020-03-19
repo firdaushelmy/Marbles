690f738d5895e1e70fabf6d864f39971b8fb2f43
Zeftiroth <zeft.huisen@gmail.com>
2020-03-19 16:57:53 +0800

    started working on threads

---
+++ src/components/threads.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import "./threads.css"
+
+function Threads() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Threads
