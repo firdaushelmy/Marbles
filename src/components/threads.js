e166ea3253c29bd1ce666e0dbc1d068b3b46f3ee
Zeftiroth <zeft.huisen@gmail.com>
2020-03-19 17:13:51 +0800

    started working on threads

--- src/components/threads.js
+++ src/components/threads.js
@@ -4,9 +4,51 @@ import axios from "axios";
 import "./threads.css"
 
 function Threads() {
+    const [content, setContent] = useState("")
+    const [template, setTemplate] = useState("")
+
+    const handleContentChange = (e) => {
+        let text = e.target.value
+        setContent(text)
+    }
+
+    const handleTemplateChange = (e) => {
+        let img = e.target.value
+        setTemplate(img)
+    }
+
+    const handleContentSubmit = () => {
+        axios.post(`https://marbles-backend.herokuapp.com/api/v1/threads/new/`, {
+            content: content,
+            user: userID,
+            template: template
+        }).then(response => {
+            console.log(response.data)
+        })
+    }
+
+
     return (
-        <div>
-            
+        <div className="container-fluid">
+            <form onSubmit={handleContentSubmit} >
+                <div>
+                    <input id="commentText" value={content} onChange={handleContentChange()} type="text" placeholder="Write your post here">
+
+                    </input>
+                
+                </div>
+                <div>
+                    <input>
+                    </input>
+                    insert picture/templates here
+                </div>
+                <div className="btn btn-outline-warning border-0" type="submit" >
+                    Post Thread
+                </div>
+
+            </form>
+
+
         </div>
     )
 }
