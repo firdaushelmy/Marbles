6b6e6251ec6f7a29dd4b646990c4e3c8708a1263
Jyn-Mun <jynmun@gmail.com>
2020-03-22 22:07:08 +0800

    panic button

--- src/panic.js
+++ src/panic.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./panic.css";
 
 function Panic() {
+  function handleClick(e) {
+    e.preventDefault();
+    let Panic = document.querySelector("#PanicSurrounding")
+    // Panic.append("Okay")
+    // Panic.style.display = "";
+    Panic.style.position = "absolute";
+    Panic.style.left = Math.floor(Math.random() * 100) + "px";
+    Panic.style.top = Math.floor(Math.random() * 100) + "px";
+    Panic.style.right = Math.floor(Math.random() * 100) + "px";
+  }
+
   return (
     <div className="container" id="PanicSurrounding">
       <div className="PanicSurround1">
         <div className="PanicSurround2">
-          <div className="Panic">
-            <a href="#">
-              PANIC</a>
-          </div>
+          <button className="Panic" onClick={handleClick}>
+            PANIC
+           </button>
         </div>
       </div>
-    </div >
+    </div>
   )
 }
 
