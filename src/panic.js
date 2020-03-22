9bfbbeec95667b6feec34aedae6be996870c2576
Jyn-Mun <jynmun@gmail.com>
2020-03-22 19:59:52 +0800

    css-ed the panic button

--- src/panic.js
+++ src/panic.js
@@ -4,12 +4,15 @@ import "./panic.css";
 function Panic() {
   return (
     <div className="container" id="PanicSurrounding">
-      <div className="Panic">
-        <a href="#">
-          PANIC
-      </a>
+      <div className="PanicSurround1">
+        <div className="PanicSurround2">
+          <div className="Panic">
+            <a href="#">
+              PANIC</a>
+          </div>
+        </div>
       </div>
-    </div>
+    </div >
   )
 }
 
