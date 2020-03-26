eebb2b20dedd52d578be93c7efff51c8bbe09f9b
Zeftiroth <zeft.huisen@gmail.com>
2020-03-26 15:11:27 +0800

    attempting to fix instant update of comments n buttons

--- src/components/comments.js
+++ src/components/comments.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 import "./comments.css"
+import CommentLikes from "./commentlikes"
 
 function Comments(threads, threadId, userID) {
     const [text, setText] = useState("")
@@ -9,32 +10,53 @@ function Comments(threads, threadId, userID) {
     console.log(threads.threadId)
     console.log(threads)
     console.log(allComments)
-   
+    const [btnDisabled,setBtnDisabled] = useState(true)
+    
+    const btnState = () => {
+        
+        const button = (document.getElementById("cmtBtn"));
+        button.disabled = btnDisabled
+        if (text != "") {
+            button.disabled = false
+            setBtnDisabled(false)
+        } 
+        
+    }
+    
+    
+    
     const handleTextChange = (e) => {
+        
         let tt = e.target.value
         console.log(tt)
         // let txt = document.getElementById("commentText").append(tt)
         setText(tt)
     }
- console.log(text);
+    console.log(text);
+    const onChange = (e) => {
+        btnState()
+        handleTextChange(e)
+    }
     useEffect(() => {
         axios
-          .get(
+        .get(
             `https://marbles-backend.herokuapp.com/api/v1/comments/${threads.threadId}`
           )
           .then(response => {
               let com = response.data.comments;
               let comm = com.sort(function(a, b) {return b.id - a.id})
-              
+              console.log(comm)
             setAllComments(comm);
           });
 
-
-
+          
     }, [])
 console.log(localStorage.getItem("user"));
     const handleTextSubmit = (e) => {
         e.preventDefault()
+        
+
+
         axios
           .post(
             `https://marbles-backend.herokuapp.com/api/v1/comments/new/${threads.threadId}`,
@@ -69,16 +91,16 @@ console.log(localStorage.getItem("user"));
     return (
         <div className="container-fluid">
             
-
+            <CommentLikes ID={threads.threadId} />
 
             <div>
                 <form onSubmit={handleTextSubmit} >
                     <div>
-                        <input id="commentText" value={text} onChange={handleTextChange} type="text" placeholder="Write some encouragement here">
+                        <input id="commentText" value={text} onChange={onChange} type="text" placeholder="Write some encouragement here">
 
                         </  input>
                     </div>
-                    <button className="btn btn-outline-warning border-0" type="submit button" >
+                    <button id="cmtBtn" className="btn btn-outline-warning border-0" type="submit button" >
                         Encourage
                 </button>
 
