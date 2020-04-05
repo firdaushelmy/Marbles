8d9084ba6a90bc89864549ab48a270fdadf34115
Zeftiroth <zeft.huisen@gmail.com>
2020-04-05 15:16:35 +0800

    =D

--- src/panic.js
+++ src/panic.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import "./panic.css";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios"
 
 function Panic() {
   function handleClick(e) {
@@ -12,12 +15,21 @@ function Panic() {
     Panic.style.top = Math.floor(Math.random() * 100) + "px";
     Panic.style.right = Math.floor(Math.random() * 100) + "px";
   }
+  const cuID = JSON.parse(localStorage.getItem('user'))
+  const cID = cuID.id
+  const handleMailgun = (e) => {
+    e.preventDefault()
+    axios.post(`https://marbles-backend.herokuapp.com/api/v1/emergencies/panic/${cID}`).then(res => {
+      console.log(res.data)
+      toast.success("Please calm down! Emergency contact has been notify about the button press")
+    })
+  }
 
   return (
     <div className="container" id="panicSurrounding">
       <div className="panicSurround1">
         <div className="panicSurround2">
-          <button className="panic">
+          <button className="panic" onClick={handleMailgun}>
             PANIC
            </button>
         </div>
