fe0e493e257f029738baeae3ce0a344764b0c156
Jyn-Mun <jynmun@gmail.com>
2020-04-07 02:09:32 +0800

    color themes for all moods

--- src/feelings.js
+++ src/feelings.js
@@ -6,14 +6,29 @@ import styled, { ThemeProvider, keyframes, withTheme } from 'styled-components';
 
 
 function Feelings(props) {
-  const { theme, setTheme, depressedTheme } = props
-  // function DepressedColorScheme() {
-  //   setTheme(depressedTheme);
-  // }
-  const DepressedColorScheme = () => {
-    setTheme(depressedTheme)
+  const { theme, setTheme, depressedTheme, defaultTheme, angryTheme, worriedTheme, reflectiveTheme, otherTheme } = props
+  const DepressedColorScheme = (e) => {
+    e.preventDefault();
+    setTheme(depressedTheme);
   };
-  console.log(DepressedColorScheme);
+  const DefaultColorScheme = () => {
+    setTheme(defaultTheme)
+  };
+  const AngryColorScheme = () => {
+    setTheme(angryTheme)
+  };
+
+  const WorriedColorScheme = () => {
+    setTheme(worriedTheme)
+  }
+
+  const ReflectiveColorScheme = () => {
+    setTheme(reflectiveTheme)
+  }
+
+  const OtherColorScheme = () => {
+    setTheme(otherTheme)
+  }
 
   const HowYouDoinDiv = styled.div`
     color: ${props => props.theme.howYouDoinDivCol};
@@ -56,17 +71,17 @@ function Feelings(props) {
           {/* <div className="col-lg-6 col-md-12 col-sm-12 d-flex"> */}
           <div className="col-md-4 col-sm-6 col-xs-6">
             <EmotionDiv className="emotion" id="depressed" onClick={DepressedColorScheme}>
-              <FeelingDelayLink delay="1000" tag={Link} to="/home" className="feelingType">
+              <FeelingDelayLink delay="1000" tag={Link} to="/home" className="feelingType" >
                 Depressed</FeelingDelayLink>
             </EmotionDiv>
           </div>
           <div className="col-md-4 col-sm-6 col-xs-6">
-            <EmotionDiv className="emotion" id="angry">
+            <EmotionDiv className="emotion" id="angry" onClick={AngryColorScheme}>
               <FeelingDelayLink delay="1000" tag={Link} to="/home" className="feelingType">Angry</FeelingDelayLink>
             </EmotionDiv>
           </div>
           <div className="col-md-4 col-sm-6 col-xs-6">
-            <EmotionDiv className="emotion" id="heartbroken">
+            <EmotionDiv className="emotion" id="heartbroken" onClick={DefaultColorScheme}>
               <FeelingDelayLink delay="1000" tag={Link} to="/home" className="feelingType">Heartbroken</FeelingDelayLink>
             </EmotionDiv>
           </div>
@@ -74,17 +89,17 @@ function Feelings(props) {
         {/* <div className="col-lg-6 col-md-12 col-sm-12 d-flex"> */}
         <div className="container-fluid row justify-content-center align-items-center">
           <div className="col-md-4 col-sm-6 col-xs-6">
-            <EmotionDiv className="emotion" id="worried">
+            <EmotionDiv className="emotion" id="worried" onClick={WorriedColorScheme}>
               <FeelingDelayLink delay="1000" tag={Link} to="/home" className="feelingType">Worried</FeelingDelayLink>
             </EmotionDiv>
           </div>
           <div className="col-md-4 col-sm-6 col-xs-6">
-            <EmotionDiv className="emotion" id="reflective">
+            <EmotionDiv className="emotion" id="reflective" onClick={ReflectiveColorScheme}>
               <FeelingDelayLink delay="1000" tag={Link} to="/home" className="feelingType">Reflective</FeelingDelayLink>
             </EmotionDiv>
           </div>
           <div className="col-md-4 col-sm-6 col-xs-6">
-            <EmotionDiv className="emotion" id="other">
+            <EmotionDiv className="emotion" id="other" onClick={OtherColorScheme}>
               <FeelingDelayLink delay="1000" tag={Link} to="/home" className="feelingType">Other</FeelingDelayLink>
             </EmotionDiv>
           </div>
